3741db2241859b24268a655df24ed1dd61a37720
graysonw15 <37517330+graysonw15@users.noreply.github.com>
2020-04-21 13:08:17 -0400

    added moduleTest.py

---
+++ moduleTest.py
@@ -0,0 +1,15 @@
+import evenOdd
+
+
+evenOdd.adder()
+
+
+nums = [1,2,3,4,5,6]
+names = ['Jacob', 'James', 'Jack']
+values = [nums, names]
+print(values)
+nums.append(45)
+
+print(values)
+nums.insert(2,30)
+print(values)
