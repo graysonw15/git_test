bbdf524a4bb57965749c072418706d81076c2aef
graysonw15 <37517330+graysonw15@users.noreply.github.com>
2020-04-21 13:03:51 -0400

    added evenOdd.py

---
+++ evenOdd.py
@@ -0,0 +1,15 @@
+def adder():
+    # Store input numbers
+    num1 = input('Enter first number: ')
+    num2 = input('Enter second number: ')
+
+    # Add two numbers
+    sum1 = float(num1) + float(num2)
+
+    # Display the sum
+    print('The sum of {0} and {1} is {2}'.format(num1, num2, sum1))
+    sum2 = 26
+    return sum1, sum2
+
+
+
